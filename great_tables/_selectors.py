9a0a565cbb2303b1cf01d6fc1f452e4f7cfb4d9a
jrycw <jerry@ycwu.space>
2025-04-28 00:43:55 +0800

    Update type hints in `_selectors.py`

--- great_tables/_selectors.py
+++ great_tables/_selectors.py
@@ -1,5 +1,14 @@
 from functools import singledispatch
-from ._tbl_data import DataFrameLike, PdDataFrame, PlDataFrame, PyArrowTable, _raise_not_implemented
+from ._tbl_data import (
+    DataFrameLike,
+    PdDataFrame,
+    PdSeries,
+    PlDataFrame,
+    PlSeries,
+    PyArrowArray,
+    PyArrowTable,
+    _raise_not_implemented,
+)
 
 
 class GTSelector: ...
@@ -29,7 +38,7 @@ class every_n_row(GTRowSelector):
         return _every_n_row(data, self._n, self._offset)
 
 
-def _get_bool_list(n_rows: int, n: int, offset: int) -> list[int]:
+def _get_bool_list(n_rows: int, n: int, offset: int) -> list[bool]:
     if n > n_rows:
         raise ValueError("`n` must not exceed `n_rows`.")
     return [True if ((i % n) == offset) else False for i in range(n_rows)]
@@ -41,7 +50,7 @@ def _every_n_row(data: DataFrameLike, n: int, offset: int) -> DataFrameLike:
 
 
 @_every_n_row.register(PdDataFrame)
-def _(data: PdDataFrame, n: int, offset: int) -> list[int]:
+def _(data: PdDataFrame, n: int, offset: int) -> PdSeries:
     import pandas as pd
 
     n_rows = data.shape[0]
@@ -49,7 +58,7 @@ def _(data: PdDataFrame, n: int, offset: int) -> list[int]:
 
 
 @_every_n_row.register(PlDataFrame)
-def _(data: PlDataFrame, n: int, offset: int) -> list[int]:
+def _(data: PlDataFrame, n: int, offset: int) -> PlSeries:
     import polars as pl
 
     n_rows = data.height
@@ -57,7 +66,7 @@ def _(data: PlDataFrame, n: int, offset: int) -> list[int]:
 
 
 @_every_n_row.register(PyArrowTable)
-def _(data: PyArrowTable, n: int, offset: int) -> list[int]:
+def _(data: PyArrowTable, n: int, offset: int) -> PyArrowArray:
     import pyarrow as pa
 
     n_rows = data.num_rows
