7caff4e12ec6661a3a1617c82e970604fd260488
jrycw <jerry@ycwu.space>
2025-04-28 00:45:11 +0800

    Simplify tests in `test_selectors.py`

--- tests/test_selectors.py
+++ tests/test_selectors.py
@@ -36,28 +36,28 @@ def df(request) -> pd.DataFrame:
     ],
 )
 def test_every_n_row(df: DataFrameLike, row_selector, expected):
-    res = GT(df).fmt_integer(columns="col1", rows=row_selector, scale_by=100)
+    res = GT(df).fmt_integer(columns="col1", rows=row_selector)
     assert len(res._formats) == 1
     assert res._formats[0].cells.rows == expected
 
 
 def test_every_n_row_raise(df: DataFrameLike):
     with pytest.raises(ValueError) as exc_info:
-        res = GT(df).fmt_integer(columns="col1", rows=every_n_row(-1), scale_by=100)
+        res = GT(df).fmt_integer(columns="col1", rows=every_n_row(-1))
     assert "`n` must be a positive integer greater than 0." in exc_info.value.args[0]
 
     with pytest.raises(ValueError) as exc_info:
-        res = GT(df).fmt_integer(columns="col1", rows=every_n_row(2, -1), scale_by=100)
+        res = GT(df).fmt_integer(columns="col1", rows=every_n_row(2, -1))
     assert "`offset` must not be a negative integer." in exc_info.value.args[0]
 
     with pytest.raises(ValueError) as exc_info:
-        res = GT(df).fmt_integer(columns="col1", rows=every_n_row(2, 2), scale_by=100)
+        res = GT(df).fmt_integer(columns="col1", rows=every_n_row(2, 2))
     assert "`offset` must be less than `n_rows`." in exc_info.value.args[0]
 
     with pytest.raises(ValueError) as exc_info:
-        res = GT(df).fmt_integer(columns="col1", rows=every_n_row(2, 3), scale_by=100)
+        res = GT(df).fmt_integer(columns="col1", rows=every_n_row(2, 3))
     assert "`offset` must be less than `n_rows`." in exc_info.value.args[0]
 
     with pytest.raises(ValueError) as exc_info:
-        res = GT(df).fmt_integer(columns="col1", rows=every_n_row(100), scale_by=100)
+        res = GT(df).fmt_integer(columns="col1", rows=every_n_row(100))
     assert "`n` must not exceed `n_rows`." in exc_info.value.args[0]
